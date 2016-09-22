525da0b6c03e30cfa7ebf99e91267905730795e9
syn <synwe@yahoo.fr>
2016-09-22 10:40:46 +0000

    Django 1.10 urls format

--- mcat/urls.py
+++ mcat/urls.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from mcat.views import ProductView, CategoryHomeView, CategoryView, ProductsInCategoryView, SearchView
 from mcat.conf import USE_ORDER
 
 if USE_ORDER:
-    urlpatterns = patterns('', 
+    urlpatterns = [ 
     url('^order/', include('mcat_order.urls')),
     url(r'^product/(?P<category_slug>[-_\w]+)/(?P<slug>[-_\w]+)/$', ProductView.as_view(), name="product-detail"),
     url(r'^products/(?P<slug>[-_\w]+)/$', ProductsInCategoryView.as_view(), name="product-list"),
     url(r'^search/$', SearchView.as_view(), name="product-search"),
     url(r'^(?P<slug>[-_\w]+)/$', CategoryView.as_view(), name="category-list"),
     url(r'^', CategoryHomeView.as_view(), name="category-home"),
-    )
+    ]
 else:
-    urlpatterns = patterns('', 
+    urlpatterns = [ 
     url(r'^product/(?P<category_slug>[-_\w]+)/(?P<slug>[-_\w]+)/$', ProductView.as_view(), name="product-detail"),
     url(r'^products/(?P<slug>[-_\w]+)/$', ProductsInCategoryView.as_view(), name="product-list"),
     url(r'^search/$', SearchView.as_view(), name="product-search"),
     url(r'^(?P<slug>[-_\w]+)/$', CategoryView.as_view(), name="category-list"),
     url(r'^', CategoryHomeView.as_view(), name="category-home"),
-    )
+    ]
 
